b6d50594f0a6ef752432135358f2d47f59980e4e
Rafa de Castro <rafael@micubiculo.com>
2012-05-30 20:21:59 +0200

    Clearing canvas before a new round

--- public/javascripts/game-gui.js
+++ public/javascripts/game-gui.js
@@ -126,4 +126,5 @@ var clearCanvas = function (canvas) {
   ctx.setTransform(1, 0, 0, 1, 0, 0);
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   ctx.restore();
+  $("#drawing_zone").sketch().draw();
 }
